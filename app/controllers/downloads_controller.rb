b5ab9c871d74cbf7fe5d1be91642298421c969f4
“Willy <will@rubify.com>
2011-01-12 20:12:36 +0800

    bwahahaha

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -3,7 +3,9 @@ class DownloadsController < ApplicationController
   
   def welcome
     show_two_face
-    
+    if params[:new_user]
+      puts "hehehe, we are with new user \n"*200
+    end
   end
   
   
@@ -11,6 +13,7 @@ class DownloadsController < ApplicationController
   def show_two_face
     if current_user
       @downloads = current_user.downloads
+      @new_user = User.new 
       render :file => "/downloads/show_files.html.erb"  
     else
       render :file => "/downloads/welcome.html.erb"
